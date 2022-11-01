8062e6dbb51490973d5481a44bbab9071c18edd2
paul <paul.novac@continental-corporation.com>
2022-11-01 13:32:20 +0200

    created home & profile page

---
+++ website/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+
+
+    @app.route("/")
+    def home():
+        return "<h1>Home Page</h1>"
+
+    @app.route("/profile")
+    def profile():
+        return "<h1>Profile page</h1>"
+
+
+    return app
